41401a2838b4d17d7fd4215b9469f4db87849cdd
Lucas <contatolucasoficial0@gmail.com>
2023-09-04 18:45:06 -0300

    Commit 3

---
+++ src/Cliente.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+/*
+public class Cliente extends Pessoa {
+
+}
+*/
--- src/Testes.java
+++ src/Testes.java
@@ -1,6 +1,6 @@
 public class Testes {
     public static void main(String[] args) {
-        Pessoa p1 = new Pessoa("Lucas", "(82)98201-6164", "15/10/2000");
+        Pessoa p1 = new Pessoa("João", "(82)97523-2864", "22/11/2000");
         System.out.println(p1.getNome());
         System.out.println(p1.getNumero());
         System.out.println(p1.getDataDeNascimento());
