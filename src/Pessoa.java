257b49b69ad3581e56506e2ae2f05ac3b0f8b624
Lucas <contatolucasoficial0@gmail.com>
2023-09-04 17:22:13 -0300

    Commit 1

---
+++ src/Pessoa.java
@@ -0,0 +1,50 @@
+public class Pessoa {
+    private String nome;
+    private String numero;
+    private String dataDeNascimento;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(String dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public Pessoa(String nome, String numero, String dataDeNascimento){
+        this.setNome(nome);
+        if (formatoAdequadoDeNumero(numero)) {
+            this.setNumero(numero);
+        } else throw new RuntimeException("Formato de número inválido.");
+        if (formatoAdequadoDeData(dataDeNascimento)) {
+            this.setDataDeNascimento(dataDeNascimento);
+        } else throw new RuntimeException("Formato de data inválido.");
+    }
+
+    private boolean formatoAdequadoDeNumero(String numero){
+        String regex = "(\\()?(\\d{2})?(\\))?(\\)?9?\\d{4}-?\\d{4})";
+        return numero.matches(regex);
+    }
+
+    private boolean formatoAdequadoDeData(String data){
+        String regex = "^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/(\\d{4})$";
+        return data.matches(regex);
+    }
+}
---
+++ src/Testes.java
@@ -0,0 +1,8 @@
+public class Testes {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("Lucas", "(82)98201-6164", "15/10/2000");
+        System.out.println(p1.getNome());
+        System.out.println(p1.getNumero());
+        System.out.println(p1.getDataDeNascimento());
+    }
+}
